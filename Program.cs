d8aedb53c8a400f8b4d80156530128fe2f65ce52
Mahajack <59824635+Mahajack@users.noreply.github.com>
2020-04-08 18:00:49 +0530

    SelectionSort

--- Program.cs
+++ Program.cs
@@ -1,70 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System .IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace writecsvfile
+class SelectionSort
 {
-    class Program
+    static void sort(int[] arr)
     {
-        static void Main(string[] args)
-        {
-            //addRecord("124", "mercy", "56", "cake.txt");
-            Console.WriteLine(string.Join("", readRecord("124", "cake.txt", 1)));
-        }
-        public static void addRecord(string ID,string name,string age,string filepath)
-        {
-            try
-            {
-                using(System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath,true))
-                {
-                    file.WriteLine(ID + "," + name + "," + age);
-                }
-           
-            }
-            catch(Exception ex)
-            {
-                throw new ApplicationException("this program did an oopsie", ex);
-            }
-        }
-        public static string[] readRecord(string searchTerm,string filepath,int positionofsearchTerm)
-        {
-            positionofsearchTerm--;
-            string[] recordNotFound = { "Record not found" };
+        int input = arr.Length;
 
-            try
-            {
-                string[] lines = System.IO.File.ReadAllLines(@filepath);
-                
-                for (int i = 0;i < lines.Length;i++)
-                {
-                    string[] fields = lines[i].Split(',');
-                    if (recordMatches(searchTerm, fields, positionofsearchTerm))
-                    {
-                        Console.WriteLine("Record found");
-                        return fields;
-                    }
-                }
-
-                return recordNotFound;
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("This program did not an oopsie");
-                return recordNotFound;
-                throw new ApplicationException("This program did an oopsie :" ,ex);
-            }
-        }
-
-        public static bool recordMatches(string searchTerm, string[] record, int positionofsearchTerm)
+        // One by one move boundary of unsorted subarray 
+        for (int first_element = 0; first_element < input - 1; first_element++)
         {
-            if(record[positionofsearchTerm].Equals(searchTerm))
-            {
-                return true;
-            }
-            return false;
-        } 
+            // Find the minimum element in unsorted array 
+            int min_idx = first_element;
+            for (int second_element = first_element + 1; second_element < input; second_element++)
+                if (arr[second_element] < arr[min_idx])
+                    min_idx = second_element;
+
+            // Swap the found minimum element with the first element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[first_element];
+            arr[first_element] = temp;
+        }
     }
+
+    // Prints the array 
+    static void printArray(int[] arr)
+    {
+        int input = arr.Length;
+        for (int first_element = 0; first_element < input; ++first_element)
+            Console.Write(arr[first_element] + " ");
+        Console.WriteLine();
+    }
+     
+    //Main Method
+    public static void Main()
+    {
+        int[] arr = { 64, 25, 12, 22, 11 };
+        sort(arr);
+        Console.WriteLine("Sorted array");
+        printArray(arr);
+    }
+
 }
+
