581fc35efdb35ca25ec87cfaa900fbd13533f954
Mahajack <59824635+Mahajack@users.noreply.github.com>
2020-04-27 18:31:23 +0530

    Pangrams

--- Program.cs
+++ Program.cs
@@ -1,49 +1,63 @@
 ﻿using System;
-
-class Triplets
+class Pangrams
 {
-    // returns true if there is 
-    // triplet with sum equal 
-    // to 'sum' present in input[]. 
-    // Also, prints the triplet 
-    static bool find3Numbers(int[] input, int arr_size, int sum)
-                           
+    // Returns true if the string 
+    // is pangram else false 
+    public static bool checkPangram(string str)
     {
-        // Fix the first 
-        // element as input[i] 
-        for (int firstelement = 0; firstelement < arr_size - 2; firstelement++)
+
+        // Create a hash table to mark the 
+        // characters present in the string 
+        // By default all the elements of 
+        // mark would be false. 
+        bool[] mark = new bool[26];
+
+        // For indexing in mark[] 
+        int index = 0;
+
+        //Transverse the all character 
+        for (int character = 0; character < str.Length; character++)
         {
+            // If uppercase character, subtract 'A' 
+            // to find index. 
+            if ('A' <= str[character] && str[character] <= 'Z')
+                index = str[character] - 'A';
 
-            // Fix the second 
-            // element as input[j] 
-            for (int secondelement = firstelement + 1;secondelement < arr_size - 1; secondelement++)
-            {
+            // If lowercase character, 
+            // subtract 'a' to find 
+            // index. 
+            else if ('a' <= str[character] && str[character] <= 'z')
+                index = str[character] - 'a';
 
-                // the third number 
-                for (int thirdelement = secondelement + 1;thirdelement < arr_size; thirdelement++)
-                {
-                    if (input[firstelement] + input[secondelement] + input[thirdelement] == sum)
-                    {
-                        Console.WriteLine("Triplet is " + input[firstelement] + ", " + input[secondelement] + ", " + input[thirdelement]);
-                        return true;
-                    }
-                }
-            }
+            // If this character is other than english 
+            // lowercase and uppercase characters. 
+            else
+                continue;
+
+            mark[index] = true;
         }
 
-        return false;
+        // Return false if any 
+        // character is unmarked 
+        for (int character = 0; character <= 25; character++)
+            if (mark[character] == false)
+                return (false);
+
+        // If all characters 
+        // were present 
+        return (true);
     }
 
     // Driver Code 
     public static void Main()
     {
-        int[] input = { 1, 4, 45, 6, 10, 8 };
-        int sum = 11;
-       
-        int arr_size = input.Length;
+        Console.Write("Enter the Sentence : ");
+        string str = Console.ReadLine();
 
-        find3Numbers(input, arr_size, sum);
+        if (checkPangram(str) == true)
+            Console.WriteLine(str + " is a pangram.");
+        else
+            Console.WriteLine(str + " is not a pangram.");
     }
 }
-
 
