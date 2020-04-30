8b4e73dd78ff474a84234775af1bc6777826c045
Mahajack <59824635+Mahajack@users.noreply.github.com>
2020-04-30 18:13:18 +0530

    GameOfStone

--- Program.cs
+++ Program.cs
@@ -1,57 +1,28 @@
 ﻿using System;
 
-public class Anagram
+public class GameOfStone
 {
-
-    static int NO_OF_CHARS = 256;
-
-    /* function to check whether two strings 
-    are anagram of each other */
-    static bool areAnagram(char[] str1, char[] str2)
+    // Function that returns true if u win 
+    static bool checkWin(int noofstone)
     {
-        // Create 2 count arrays and initialize 
-        // all values as 0 
-        int[] count1 = new int[NO_OF_CHARS];
-        int[] count2 = new int[NO_OF_CHARS];
-        int num;
-
-        // For each character in input strings, 
-        // increment count in the corresponding 
-        // count array 
-        for (num = 0; num < str1.Length && num < str2.Length;
-            num++)
-        {
-            count1[str1[num]]++;
-            count2[str2[num]]++;
-        }
-
-        // If both strings are of different length. 
-        // Removing this condition will make the program 
-        // fail for strings like "aaca" and "aca" 
-        if (str1.Length != str2.Length)
-            return false;
-
-        // Compare count arrays 
-        for (num = 0; num < NO_OF_CHARS; num++)
-            if (count1[num] != count2[num])
-                return false;
-
-        return true;
+        if (noofstone % 2 != 0)
+            return true;
+        return false;
     }
 
-    /* Driver program to test to print printDups*/
-    public static void Main()
-    {
-        char[] str1 = ("csharp").ToCharArray();
-        char[] str2 = ("sharpc").ToCharArray();
+    // Driver code 
 
-        if (areAnagram(str1, str2))
-            Console.WriteLine("The two strings are"
-                            + "anagram of each other");
+    static public void Main()
+    {
+
+        Console.Write("Enter the No of Stone : ");
+        int noofstone = Convert.ToInt32(Console.ReadLine());
+
+        if (checkWin(noofstone))
+            Console.WriteLine("Yes!You won");
         else
-            Console.WriteLine("The two strings are not"
-                            + " anagram of each other");
+            Console.WriteLine("NO!You lose");
+
     }
+
 }
-
-
